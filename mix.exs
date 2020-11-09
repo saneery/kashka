c7342b755cdc9e1d8e036373a4649ec99ddf4a0e
Вадим Саттаров <v.sattarov@fun-box.ru>
2020-11-10 00:50:05 +0400

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kashka.MixProject do
   def project do
     [
       app: :kashka,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       source_url: "https://github.com/funbox/kashka",
